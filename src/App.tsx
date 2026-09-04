3ad19fc3846fe9a9a010967a83808a6064032e24
leiton <sebastian.leiton@uao.edu.co>
2026-09-03 23:38:43 -0500

    ✨ feat: Add the main logic to run the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+
 function App() {
   return (
     <>
-      <h1>BIENVENIDO A DESARROLLO DE SOFTWARE PARA MOVILES</h1>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+      </Routes>
     </>
   );
 }
