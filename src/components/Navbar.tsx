ae0f7689ce875bd338cfd72039ba9f9d02d37fae
leiton <sebastian.leiton@uao.edu.co>
2026-09-04 01:11:33 -0500

    ✨ feat: add UI components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+interface NavbarProp {
+  title: string;
+  imgUrl: string;
+  alt: string;
+}
+
+function Navbar({ title, imgUrl, alt }: NavbarProp) {
+  return (
+    <div className="relative flex items-center justify-center px-4 py-4 sm:px-6">
+      <Link to="/" className="absolute left-4 sm:left-6">
+        <button className="px-4 py-2 text-sm sm:px-5 sm:py-2.5 sm:text-base">
+          inicio
+        </button>
+      </Link>
+
+      <div className="flex items-center">
+        <img
+          src={imgUrl}
+          alt={alt}
+          className="mr-2 w-12 sm:w-16 md:w-20 mr-4"
+        />
+
+        <h1 className="text-4xl font-bold sm:text-3xl md:text-5xl ml-4">
+          {title}
+        </h1>
+      </div>
+
+      <Link to="/" className="absolute right-4 sm:right-6">
+        <button className="px-4 py-2 text-sm sm:px-5 sm:py-2.5 sm:text-base">
+          inicio
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+export default Navbar;
