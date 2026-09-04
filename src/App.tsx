9ada5eb6ad859b1532300eef684ea4472be07d67
leiton <sebastian.leiton@uao.edu.co>
2026-09-04 01:12:53 -0500

    ✨ feat: Add contacts page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Contacts } from "./pages/Contacts";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path={"/"} element={<Home />} />
+        <Route path={"/contacts"} element={<Contacts />} />
       </Routes>
     </>
   );
