5d4851021b6344a26e24b9271d134f3fbcb3d7d9
leiton <sebastian.leiton@uao.edu.co>
2026-09-04 01:10:06 -0500

    🗃️ mockdata: Add mockdata as a initial contacts data

---
+++ src/mockdata/contacts.mockdata.ts
@@ -0,0 +1,29 @@
+import type { Contact } from "../interfaces/contact.interface.ts";
+
+export const initialContacts: Contact[] = [
+  {
+    id: 1,
+    name: "Arthur Morgan",
+    phone: "300 456 7890",
+  },
+  {
+    id: 2,
+    name: "Daniela Sotelo",
+    phone: "315 678 2345",
+  },
+  {
+    id: 3,
+    name: "Eren Jeager",
+    phone: "301 234 5678",
+  },
+  {
+    id: 4,
+    name: "David Martínez",
+    phone: "320 789 4561",
+  },
+  {
+    id: 5,
+    name: "Laura Aura",
+    phone: "310 345 6789",
+  },
+];
