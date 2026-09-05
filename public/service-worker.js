390c97100cbaa1f3d356d371da875487c3707170
leiton <sebastian.leiton@uao.edu.co>
2026-09-05 12:06:02 -0500

    🍻 Add: Add the PWA config

---
+++ public/service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "react-pwa-v2";
+
+const APP_SHELL = ["/", "/index.html", "/manifest.json", ""];
+
+const sw = self;
+
+sw.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)),
+  );
+});
+
+sw.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key)),
+        ),
+      ),
+  );
+});
+
+// Network First
+sw.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, clone);
+        });
+
+        return response;
+      })
+      .catch(async () => {
+        const cachedResponse = await caches.match(event.request);
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        throw new Error("Recurso no encontrado en la caché");
+      }),
+  );
+});
