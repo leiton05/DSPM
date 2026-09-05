ea6f41c560e2462650efd34bc2d1136e7b5a38e9
leiton <sebastian.leiton@uao.edu.co>
2026-09-05 15:25:51 -0500

    ✨ feat: Update service-worker to implement a hybrid strategy

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,14 @@
 const CACHE_NAME = "react-pwa-v2";
 
-const APP_SHELL = ["/", "/index.html", "/manifest.json", ""];
+const APP_SHELL = ["/", "/index.html", "/manifest.json"];
 
-const sw = self;
-
-sw.addEventListener("install", (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)),
   );
 });
 
-sw.addEventListener("activate", (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches
       .keys()
@@ -24,29 +22,64 @@ sw.addEventListener("activate", (event) => {
   );
 });
 
-// Network First
-sw.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+
+  if (url.pathname.startsWith("/api/no-cache")) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  if (url.pathname.includes("/assets/static-logo.png")) {
+    event.respondWith(caches.match(event.request));
+    return;
+  }
+
+  if (
+    event.request.destination === "style" ||
+    event.request.destination === "script"
+  ) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        const fetchPromise = fetch(event.request).then((networkResponse) => {
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+          });
+          return networkResponse;
+        });
+        return cachedResponse || fetchPromise;
+      }),
+    );
+    return;
+  }
+
+  if (event.request.destination === "image") {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) return cachedResponse;
+
+        return fetch(event.request).then((networkResponse) => {
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+          });
+          return networkResponse;
+        });
+      }),
+    );
+    return;
+  }
 
   event.respondWith(
     fetch(event.request)
       .then((response) => {
         const clone = response.clone();
-
         caches.open(CACHE_NAME).then((cache) => {
           cache.put(event.request, clone);
         });
-
         return response;
       })
-      .catch(async () => {
-        const cachedResponse = await caches.match(event.request);
-
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        throw new Error("Recurso no encontrado en la caché");
-      }),
+      .catch(() => caches.match(event.request)),
   );
 });
