b03eb4aa1db960530b529cefc60da7e6e60893f7
leiton <sebastian.leiton@uao.edu.co>
2026-09-11 01:31:32 -0500

    ✨ feat: Implement the route to the login and the auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Tasks } from "./pages/Tasks";
+import Login from "./pages/Login";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
   return (
     <>
       <Routes>
+        {/* Rutas publicas */}
         <Route path={"/"} element={<Home />} />
-        <Route path={"/tasks"} element={<Tasks />} />
+        <Route path={"/login"} element={<Login />} />
+
+        {/* Rutas privadas */}
+        <Route element={<PrivateRoute />}>
+          <Route path={"/tasks"} element={<Tasks />} />
+        </Route>
       </Routes>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import { AuthProvider } from "./context/authContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>,
 );
 
